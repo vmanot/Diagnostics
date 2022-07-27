7728b7d9e38c5693960697305fec457faaeb202d
Vatsal Manot <vatsal.manot@yahoo.com>
2022-07-28 02:17:02 +0530

    Update package

--- Sources/Intramodular/Core/Loggable.swift
+++ Sources/Intramodular/Core/Loggable.swift
@@ -8,3 +8,33 @@ import Swift
 public protocol Loggable {
     var logger: PassthroughLogger { get }
 }
+
+// MARK: - Implementation -
+
+private var logger_objcAssociationKey: UInt = 0
+
+extension Loggable {
+    public var logger: PassthroughLogger {
+        PassthroughLogger(source: PassthroughLogger.Source(self))
+    }
+}
+
+extension Loggable where Self: AnyObject {
+    public var logger: PassthroughLogger {
+        if let result = objc_getAssociatedObject(self, &logger_objcAssociationKey) as? PassthroughLogger {
+            return result
+        } else {
+            objc_sync_enter(self)
+            
+            defer {
+                objc_sync_exit(self)
+            }
+
+            let result = PassthroughLogger(source: PassthroughLogger.Source(self))
+            
+            objc_setAssociatedObject(self, &logger_objcAssociationKey, result, .OBJC_ASSOCIATION_RETAIN)
+            
+            return result
+        }
+    }
+}
--- Sources/Intramodular/Loggers/PassthroughLogger.swift
+++ Sources/Intramodular/Loggers/PassthroughLogger.swift
@@ -6,21 +6,25 @@ import Combine
 import Swallow
 
 /// A logger that broadcasts its entries.
-public final class PassthroughLogger: @unchecked Sendable, Initiable, LoggerProtocol, ObservableObject {
+public final class PassthroughLogger: @unchecked Sendable, LoggerProtocol, ObservableObject {
     public typealias LogLevel = ClientLogLevel
     public typealias LogMessage = Message
     
     public struct Message: Equatable, CustomStringConvertible, LogMessageProtocol {
         public typealias StringLiteralType = String
         
-        private var value: String
-        
-        public init(stringLiteral value: String) {
-            self.value = value
-        }
+        private var rawValue: String
         
         public var description: String {
-            return self.value
+            rawValue
+        }
+
+        public init(rawValue: String) {
+            self.rawValue = rawValue
+        }
+        
+        public init(stringLiteral value: String) {
+            self.rawValue = value
         }
     }
     
@@ -29,18 +33,54 @@ public final class PassthroughLogger: @unchecked Sendable, Initiable, LoggerProt
         public let message: String
     }
     
+    public struct Source: CustomStringConvertible {
+        public enum Storage {
+            case something(Any)
+            case object(Weak<AnyObject>)
+        }
+        
+        private let storage: Storage
+        
+        public var description: String {
+            switch storage {
+                case .something(let value):
+                    return String(describing: value)
+                case .object(let object):
+                    if let object = object.value {
+                        return String(describing: object)
+                    } else {
+                        return "(null)"
+                    }
+            }
+        }
+        
+        public init(_ value: Any) {
+            if isClass(type(of: value)) {
+                self.storage = .object(Weak(value as AnyObject))
+            } else {
+                self.storage = .something(value)
+            }
+        }
+        
+        public init(_ object: AnyObject) {
+            self.storage = .object(Weak(object))
+        }
+    }
+    
     private let lock = OSUnfairLock()
+    
+    public let source: Source
     
     public internal(set) var entries: [LogEntry] = []
     
-    public init() {
-        
+    public init(source: Source) {
+        self.source = source
     }
     
     public func log(
         level: LogLevel,
         _ message: @autoclosure () -> LogMessage,
-        metadata: @autoclosure () -> [String : Any]?,
+        metadata: @autoclosure () -> [String: Any]?,
         file: String,
         function: String,
         line: UInt
@@ -57,30 +97,29 @@ public final class PassthroughLogger: @unchecked Sendable, Initiable, LoggerProt
                     message: message().description
                 )
             )
+            
+            if PassthroughLoggerGlobalConfiguration.outputToConsole {
+                print("[\(source.description)] \(message())")
+            }
         }
     }
     
     public func log(
         level: LogLevel,
         _ message: @autoclosure () -> String,
-        metadata: @autoclosure () -> [String : Any]?,
+        metadata: @autoclosure () -> [String: Any]?,
         file: String,
         function: String,
         line: UInt
     ) {
-        lock.withCriticalScope {
-            entries.append(
-                LogEntry(
-                    sourceCodeLocation: SourceCodeLocation(
-                        file: file,
-                        function: function,
-                        line: line,
-                        column: nil
-                    ),
-                    message: message()
-                )
-            )
-        }
+        log(
+            level: level,
+            LogMessage(rawValue: message()),
+            metadata: metadata(),
+            file: file,
+            function: function,
+            line: line
+        )
     }
 }
 
@@ -94,6 +133,32 @@ extension PassthroughLogger: TextOutputStream {
             await MainActor.run {
                 self.objectWillChange.send()
             }
+        }
+    }
+}
+
+// MARK: - Auxiliary Implementation -
+
+enum PassthroughLoggerGlobalConfiguration {
+    @TaskLocal
+    static var outputToConsole: Bool = false
+}
+
+
+extension PassthroughLogger {
+    public static func outputToConsole(
+        _ body: () throws -> Void
+    ) rethrows {
+        try PassthroughLoggerGlobalConfiguration.$outputToConsole.withValue(true) {
+            try body()
+        }
+    }
+    
+    public static func outputToConsole(
+        _ body: () async throws -> Void
+    ) async rethrows {
+        try await PassthroughLoggerGlobalConfiguration.$outputToConsole.withValue(true) {
+            try await body()
         }
     }
 }
